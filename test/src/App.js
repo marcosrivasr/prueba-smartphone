21f03f686f13e6e1ea602d18b18a3fb21d02a349
Marcos Rivas R @vidamrr <marcosrivasr@gmail.com>
2021-06-15 23:04:58 +0000

    todos app implementation

--- test/src/App.js
+++ test/src/App.js
@@ -2,9 +2,43 @@
 import './App.css';
 
 function App() {
+
+  const [items, setItems] = useState({});
+  const [text, setText] = useState('');
+
+  function addItem(text){
+    const todos = [...items];
+    todos.unshift({text: text});
+    setItems([...todos]);
+  }
+
+  function handleChange(e){
+    const value = e.target.value;
+
+    setText(value);
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    addItem(text);
+  }
+
   return (
     <div className="App">
-      <h1>Hola desde mi smartphone</h1>
+      <h1>Todos</h1>
+
+      <form onSubmit={handleSubmit}>
+      </form>
+      <input type="text" onChange={handleChange} value={text} />
+
+      <div>
+        {
+          items.map((item, index) => {
+            return <li key={index}>{item}</li>
+            
+          } )
+        }
+      </div>
     </div>
   );
 }
